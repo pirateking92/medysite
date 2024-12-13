3966ba4de6e390a909de7fbb1da3c73b40750a61
Matt Doyle <doyle9214@gmail.com>
2024-12-13 16:51:43 +0000

    changes to styling in the card so that the card has better readability

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -56,10 +56,10 @@ export default function HeroSection() {
                  mx-auto
         "
       >
-        <SmokeFadeIn>
+        <SmokeFadeIn visibleOnLoad={false}>
           <Card
             className="w-full
-                     md:w-1/2
+                     
                      max-w-xl
                      shadow-lg
                      border
@@ -73,32 +73,43 @@ export default function HeroSection() {
                 Medy D'Avino
               </CardTitle>
             </CardHeader>
-            <CardDescription className="px-6 pt-2 text-gray-600 prose-lg">
-              Translator, Interpreter - Italian, English, Spanish the face
-              behind MD Linguistic Solutions
+            <CardDescription className="px-6 pt-2 text-gray-900 text-xl prose-2xl">
+              <p>Translator, Interpreter - Italian, English, Spanish </p>
+              <p>The face behind MD Linguistic Solutions</p>
             </CardDescription>
             <CardContent className="p-6">
-              <p className="text-gray-700 prose-lg">
-                driven by a genuine passion for helping people understand and be
-                understood. Medy’s passion for communication took a
-                transformative turn after a few years in the field. With a
-                natural talent for creative writing, marketing, and
-                communication, coupled with a love for entrepreneurship, she
-                realized that helping businesses reach new audiences was her
-                true calling. At the same time, her deep interest in medicine
-                and law made it challenging to settle on just one niche—she knew
-                that being a jack of all trades wouldn’t foster the credibility
-                she valued. Facing this reality, Medy chose to specialize in
-                empowering businesses through tailored language solutions, while
-                building a trusted network of colleagues to support clients in
-                other specialized areas. This collaborative vision is what led
-                to the creation of MD Linguistic Solutions, where businesses and
-                individuals alike find the expertise and support they need to
-                communicate effectively and confidently.
+              <p className="text-gray-700 prose-md mb-4">
+                Driven by a genuine passion for helping people understand and be
+                understood.
+              </p>
+
+              <p className="text-gray-700 prose-md mb-4">
+                Medy's passion for communication took a transformative turn
+                after a few years in the field. With a natural talent for
+                creative writing, marketing, and communication, coupled with a
+                love for entrepreneurship, she realized that helping businesses
+                reach new audiences was her true calling.
+              </p>
+
+              <p className="text-gray-700 prose-md mb-4">
+                At the same time, her deep interest in medicine and law made it
+                challenging to settle on just one niche—she knew that being a
+                jack of all trades wouldn't foster the credibility she valued.
+                Facing this reality, Medy chose to specialize in empowering
+                businesses through tailored language solutions, while building a
+                trusted network of colleagues to support clients in other
+                specialized areas.
+              </p>
+
+              <p className="text-gray-700 prose-md mb-4">
+                This collaborative vision is what led to the creation of MD
+                Linguistic Solutions, where businesses and individuals alike
+                find the expertise and support they need to communicate
+                effectively and confidently.
               </p>
             </CardContent>
             <CardFooter className="bg-gray-50 p-6 text-sm text-gray-500">
-              <p>foot</p>
+              <p></p>
             </CardFooter>
           </Card>
         </SmokeFadeIn>
